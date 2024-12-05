4cd3213e664284bb87059d0aebd90b3f4d27b952
HwiGyoung Lee <ter0000@naver.com>
2024-12-05 15:59:35 +0900

    🐛 fix(cors) : 잘못 설정된 origin 수정

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,18 @@
 module.exports = {
-  origin: ["http://localhost.com:3000"],
+  origin: ["http://localhost:3000"],
   methods: ["GET", "POST", "DELETE", "PUT"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Custom-Header",
+    "X-Requested-With",
+    "Referer",
+    "Cookie",
+    "Origin",
+    "Sec-CH-UA",
+    "Sec-CH-UA-Mobile",
+    "Sec-CH-UA-Full-Version-List",
+    "Sec-CH-UA-Platform",
+  ],
   credentials: true,
 };
