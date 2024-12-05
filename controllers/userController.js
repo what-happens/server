150d3001ceae9acb67ff32cbb1d8178945bbe47d
HwiGyoung Lee <ter0000@naver.com>
2024-12-05 18:41:00 +0900

    ✨ feat(userController): Firebase 로그인 및 AccessToken 쿠키 저장 로직 구현

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const admin = require("firebase-admin");
+const { getIdCookieOptions } = require("../config/cookie");
 
 const createUser = async (req, res) => {
   const errors = validationResult(req);
@@ -8,6 +9,7 @@ const createUser = async (req, res) => {
     //req에서 에러가 있을 경우
     return res.status(400).json({ errors: errors.array() });
   }
+
   const { email, password, name } = req.body;
 
   try {
@@ -24,4 +26,61 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+const loginUser = async (req, res) => {
+  const errors = validationResult(req);
+
+  console.log("errors", errors);
+  if (!errors.isEmpty()) {
+    //req에서 에러가 있을 경우
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const result = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASE_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const data = await result.json(); // 응답 데이터 파싱
+
+    // 에러 응답 처리
+    if (!result.ok) {
+      throw {
+        status: 400,
+        message: "Login Failed check Email or Password",
+      };
+    }
+    // 응답 처리 수정
+    console.log(result.ok);
+    res.cookie("accessToken", data.idToken, getIdCookieOptions);
+    res.status(200).send({
+      message: "Success Login",
+    });
+  } catch (error) {
+    if (error.status && error.message) {
+      return res.status(error.status).send({
+        message: error.message,
+        error: error.error,
+      });
+    }
+    // 일반 서버 에러 처리
+    res.status(500).send({
+      message: "Server Error",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { createUser, loginUser };
