7eb0e4596ddd10396707ad895bc5981c1a979246
HwiGyoung Lee <ter0000@naver.com>
2024-12-05 15:33:37 +0900

    ✨ feat(cors) : 연결을 허용할 도메인 설정

---
+++ config/cors.js
@@ -0,0 +1,6 @@
+module.exports = {
+  origin: ["http://localhost.com:3000"],
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
