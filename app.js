466d58edd455e932631a136cbc4e54c2574bd82b
HwiGyoung Lee <ter0000@naver.com>
2024-12-05 18:02:06 +0900

    🔥 remove : 사용하지 않는firebase client sdk 코드 제거

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 require("./config/env"); //env.js 호출하여 환경 변수 로드
+require("./config/firebase");
 
 const express = require("express");
 const cors = require("cors");
 const corsConfig = require("./config/cors");
 const routes = require("./routes"); //index.js 자동 탐색
 const { PORT } = require("./config/env");
-const { firebaseAdmin, firebaseClient } = require("./config/firebase");
 
 const app = express();
 
 app.use(cors(corsConfig));
-firebaseAdmin(); //admin 초기화
-//client 초기화  firebase는 server에서 로그인과 패스워드로 로그인을 직접 처리 불가
-//firebaseClient 로 서버에서 로그인 처리 후 idToken을 클라이언트에 넘겨주는 방식 사용
-firebaseClient();
 
 //JSON 요청을 자동으로 파싱해주는 미들웨어
 app.use(express.json());
--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,9 @@
+// firebase.js (Firebase Admin SDK 및 클라이언트 SDK 설정)
+
 const admin = require("firebase-admin");
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+// Firebase Admin SDK 초기화
 
 if (!serviceAccount) {
   throw new Error("FIREBASE_SERVICE_ACCOUNT 환경 변수가 설정되지 않았습니다.");
@@ -10,8 +14,8 @@ try {
   admin.initializeApp({
     credential: admin.credential.cert(parsedServiceAccount),
   });
-  console.log("Firebase 인증 성공");
+  console.log("Firebase 인증(Admin) 성공");
 } catch (error) {
-  console.log("Error initializing Firebase : ", error);
+  console.log("Error initializing Firebase Admin : ", error);
   process.exit(1);
 }
