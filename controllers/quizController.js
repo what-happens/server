a6ffdc2e419987a748f93a8ff7008f07b8db783b
HwiGyoung Lee <ter0000@naver.com>
2024-12-06 16:54:32 +0900

    🎨 refactor(quizController) : 변수명 수정으로 코드의 가독성 향상

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -11,8 +11,8 @@ const getQuiz = async (req, res) => {
   const { category, limit } = req.query;
 
   try {
-    const shffledQuiz = await QuizModal.getRandomQuiz(category, limit);
-    res.status(200).json({ quiz: shffledQuiz });
+    const selectedQuiz = await QuizModal.getRandomQuiz(category, limit);
+    res.status(200).json({ quiz: selectedQuiz });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
