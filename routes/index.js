8eefc55c9d3ff143ccac1f48987fb2a29d268e5f
HwiGyoung Lee <ter0000@naver.com>
2024-12-06 15:45:10 +0900

    ✨ feat(route) : quizRoute 추가

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userRoute = require("./userRoute");
-
+const quizRoute = require("./quizRoute");
 router.use("/user", userRoute);
-
+router.use("/quiz", quizRoute);
 module.exports = router;
