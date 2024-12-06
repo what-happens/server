d3abe81c8ad69ff71fd8b8884766021d6e929696
HwiGyoung Lee <ter0000@naver.com>
2024-12-06 15:44:04 +0900

    ➕ chore : cookie 를 파싱해주는 패키지 추가

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 require("./config/env"); //env.js 호출하여 환경 변수 로드
-require("./config/firebase");
-
 const express = require("express");
 const cors = require("cors");
 const corsConfig = require("./config/cors");
-const routes = require("./routes"); //index.js 자동 탐색
+const { initializeFirebaseApp } = require("./config/firebase");
 const { PORT } = require("./config/env");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+initializeFirebaseApp();
 
+const routes = require("./routes"); //index.js 자동 탐색
+
+app.use(cookieParser());
 app.use(cors(corsConfig));
-
 //JSON 요청을 자동으로 파싱해주는 미들웨어
 app.use(express.json());
-
 //모든 라우트 연결
 app.use(routes);
 
