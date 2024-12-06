1bfdc87e2f77f78b40d8ad5ed68853cff7815427
HwiGyoung Lee <ter0000@naver.com>
2024-12-06 16:57:27 +0900

    ⚡ perf(quizController): 쿼리 최적화로 Firebase 읽기 횟수 감소 (docs 수 => limit 수로 줄어듦)

--- models/quiz.js
+++ models/quiz.js
@@ -1,22 +1,29 @@
 const { getFirestoreInstance } = require("../config/firebase");
-const { shffleArray } = require("../utils/shffleArray");
+const { selectArrayElement } = require("../utils/selectArrayElement");
 const db = getFirestoreInstance();
 
 class QuizModal {
   static async getRandomQuiz(category, limit) {
     try {
-      const query = db.collection("quiz").where("category", "==", category);
-      const snapshot = await query.get(); // 현재 쿼리에 맞는 문서들을 db에서 불러옴
-      if (snapshot.empty) {
+      const categoryQuery = db.collection("category").doc(category);
+      const categorySnapshot = await categoryQuery.get(); //category 컬렉션에서 category 변수의 값과 일치하는 doc를 불러온다.
+      const qIds = categorySnapshot.data().qid;
+      const selectedQids = selectArrayElement(qIds, limit);
+      console.log("qIds", qIds, "selectedQids", selectedQids);
+      let quizQuery = db
+        .collection("quiz")
+        .where("category", "==", category)
+        .where("id", "in", selectedQids);
+      const quizSnapshot = await quizQuery.get();
+      if (quizSnapshot.empty) {
         return [];
       }
-      //스냅샷 데이터를 배열로 변환
-      const quizzes = snapshot.docs.map((doc) => ({
-        id: doc.qid,
+      const quizzes = quizSnapshot.docs.map((doc) => ({
+        id: doc.id,
         ...doc.data(),
       }));
-      const shffledQuizzes = shffleArray(quizzes).slice(0, limit);
-      return shffledQuizzes;
+      return quizzes;
+      //스냅샷 데이터를 배열로 변환
     } catch (error) {
       throw new Error(`퀴즈를 가져오지 못했습니다: ${error.message}`);
     }
