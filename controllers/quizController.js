651cb6e0b2b1df834d584718524408a67bc5d3a7
HwiGyoung Lee <ter0000@naver.com>
2024-12-06 15:41:33 +0900

    ✨ feat(controller) : db에서 선택된 퀴즈를 불러오는 기능 추가

---
+++ controllers/quizController.js
@@ -0,0 +1,21 @@
+const { validationResult } = require("express-validator");
+const QuizModal = require("../models/quiz");
+const getQuiz = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    //req의 쿼리에서 error가 있을 경우
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { category, limit } = req.query;
+
+  try {
+    const shffledQuiz = await QuizModal.getRandomQuiz(category, limit);
+    res.status(200).json({ quiz: shffledQuiz });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getQuiz };
