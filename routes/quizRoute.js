99486910ae3f6bea16f9571a0925a70ab3ea70ee
HwiGyoung Lee <ter0000@naver.com>
2024-12-10 13:12:52 +0900

    ✨ feat(quizRoute) : qid를 params로 받아 일치하는 퀴즈만 받아오는 라우트 추가

--- routes/quizRoute.js
+++ routes/quizRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { quizQueryValidator } = require("../validators/quizValidator");
-const { getQuiz } = require("../controllers/quizController");
+const {
+  quizQueryValidator,
+  quizParamValidator,
+} = require("../validators/quizValidator");
+const { getQuiz, getQuizzes } = require("../controllers/quizController");
 const authenticate = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.get("/", authenticate, quizQueryValidator, getQuiz);
+router.get("/", authenticate, quizQueryValidator, getQuizzes);
+
+router.get("/:qid", authenticate, quizParamValidator, getQuiz);
 
 module.exports = router;
