fa5c9b3cfb8e2d3d77dfaa7c3b4f4cc1eab8894a
HwiGyoung Lee <ter0000@naver.com>
2024-12-10 15:50:46 +0900

    🎨 refactoring (bookmark ): 사용하지 않는 모듈 require 제거

--- models/bookmark.js
+++ models/bookmark.js
@@ -1,6 +1,5 @@
 const { FieldValue, Timestamp } = require("firebase-admin/firestore");
 const { getFirestoreInstance } = require("../config/firebase");
-const { changeBookmarkFormat } = require("../utils/changeformat");
 const db = getFirestoreInstance();
 class Bookmark {
   static getBookmarks = async (uid) => {
@@ -8,9 +7,11 @@ class Bookmark {
       const bookmarkDoc = await db.collection("bookmark").doc(uid).get();
       if (bookmarkDoc.exists) {
         const data = bookmarkDoc.data();
-        const bookmarkData = changeBookmarkFormat(data);
-        return bookmarkData;
+        if (data.bookmark && Array.isArray(data.bookmark)) {
+          return data.bookmark;
+        }
       }
+      return [];
     } catch (error) {
       throw new Error("북마크 데이터를 가지고 오지 못했습니다.");
     }
