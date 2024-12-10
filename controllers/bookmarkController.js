e5b2f9871bf8596e80b168ad0eff938a5b1979cb
HwiGyoung Lee <ter0000@naver.com>
2024-12-10 13:08:51 +0900

    ✨ feat(bookmarkController) :북마크를 추가 또는 삭제하는 controller 추가

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -11,4 +11,21 @@ const getBookmark = async (req, res) => {
   }
 };
 
+const postBookmark = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { qid } = req.params;
+  const { uid } = req.user;
+
+  try {
+    await Bookmark.postBookmark(uid, qid);
+    res.status(200).json({ message: "북마크 업데이트 성공" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 module.exports = { getBookmark, postBookmark };
