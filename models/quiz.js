3a0fef4efb8505cdf91b7e5b89e9deaffaadd7b3
HwiGyoung Lee <ter0000@naver.com>
2024-12-10 13:12:16 +0900

    ✨ feat(quiz) : qid와 일치하는 퀴즈만 가지고 오는 기능 추가

--- models/quiz.js
+++ models/quiz.js
@@ -9,11 +9,10 @@ class QuizModal {
       const categorySnapshot = await categoryQuery.get(); //category 컬렉션에서 category 변수의 값과 일치하는 doc를 불러온다.
       const qIds = categorySnapshot.data().qid;
       const selectedQids = selectArrayElement(qIds, limit);
-      console.log("qIds", qIds, "selectedQids", selectedQids);
       let quizQuery = db
         .collection("quiz")
         .where("category", "==", category)
-        .where("id", "in", selectedQids);
+        .where("id", "in", selectedQids); //in은 한번에 10개의 처리가 가능 배치처리로 바꿔줘야한다.
       const quizSnapshot = await quizQuery.get();
       if (quizSnapshot.empty) {
         return [];
@@ -24,6 +23,18 @@ class QuizModal {
       }));
       return quizzes;
       //스냅샷 데이터를 배열로 변환
+    } catch (error) {
+      throw new Error(`퀴즈를 가져오지 못했습니다: ${error.message}`);
+    }
+  }
+
+  static async getSelectQuiz(qid) {
+    const id = parseInt(qid) + 1;
+    try {
+      const quizDoc = await db.collection("quiz").doc(`Q${id}`).get(); //qid로 값을 갖고옴
+      if (quizDoc.exists) {
+        return quizDoc.data();
+      }
     } catch (error) {
       throw new Error(`퀴즈를 가져오지 못했습니다: ${error.message}`);
     }
