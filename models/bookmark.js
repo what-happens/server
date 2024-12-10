940fb0feb62e7bdf41cc684280558fec6704f903
HwiGyoung Lee <ter0000@naver.com>
2024-12-10 13:07:03 +0900

    ✨ feat(bookmark) : 북마크 여부에 따라 제거 및 추가를 하는 메서드 postBookmark 추가

--- models/bookmark.js
+++ models/bookmark.js
@@ -15,6 +15,41 @@ class Bookmark {
       throw new Error("북마크 데이터를 가지고 오지 못했습니다.");
     }
   };
+  static postBookmark = async (uid, qid) => {
+    try {
+      const bookmarkRef = db.collection("bookmark").doc(uid);
+      const bookmarkDoc = await bookmarkRef.get();
+
+      if (bookmarkDoc.exists) {
+        const bookmarks = bookmarkDoc.data().bookmark || [];
+        const isAlreadyBookmarked = bookmarks.some(
+          (bookmark) => bookmark.qid === parseInt(qid)
+        );
+
+        if (isAlreadyBookmarked) {
+          //이미 존재하는 qid라면
+          await bookmarkRef.update({
+            bookmark: FieldValue.arrayRemove(
+              bookmarks.find((bookmark) => bookmark.qid === parseInt(qid))
+            ),
+          });
+        } else {
+          //북마크가 없는 상태라면
+          await bookmarkRef.set(
+            {
+              bookmark: FieldValue.arrayUnion({
+                qid: parseInt(qid),
+                updateAt: Timestamp.now(),
+              }),
+            },
+            { merge: true }
+          );
+        }
+      }
+    } catch (error) {
+      throw new Error(`북마크 업데이트 에러 ${error}`);
+    }
+  };
 }
 
 module.exports = { Bookmark };
