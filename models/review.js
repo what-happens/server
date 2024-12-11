89426068c4d099dcb658524dcb5a165ae8628bcf
HwiGyoung Lee <ter0000@naver.com>
2024-12-11 20:08:38 +0900

    ✨ feat(review) : constructor RoRo 패턴 적용 및 review의 doc 이 존재하는지 확인하는 방법 변경

--- models/review.js
+++ models/review.js
@@ -1,8 +1,35 @@
-const { runTransaction, Timestamp } = require("firebase-admin/firestore");
+const { Timestamp } = require("firebase-admin/firestore");
 const { getFirestoreInstance } = require("../config/firebase");
+const { compareTime } = require("../utils/comparisons");
 const db = getFirestoreInstance();
 
 class Review {
+  constructor({ qid, updateAt, isBookmark, isWrong, category }) {
+    this.qid = qid;
+    this.updateAt = updateAt;
+    this.isBookmark = isBookmark;
+    this.isWrong = isWrong;
+    this.category = category;
+  }
+
+  static getReview = async (uid) => {
+    try {
+      const reviewDoc = await db.collection("reviewNote").doc(uid).get();
+      if (reviewDoc.exists) {
+        const data = reviewDoc.data();
+        if (data.review && Array.isArray(data.review)) {
+          return data.review;
+        }
+      }
+      return [];
+    } catch (error) {
+      throw new Error("복습 노트 데이터를 가지고 오지 못했습니다.");
+    }
+  };
+
+  //model의 역할에 controller에서 처리할 부분이 보임
+  //또한 공통 로직이 존재하므로 함수로 리팩토링을 진행하는게 좋아보임
+
   static postReview = async (uid, review) => {
     try {
       const reviewRef = db.collection("reviewNote").doc(uid);
@@ -12,10 +39,14 @@ class Review {
         //덮어쓰는 로직이므로 불러오느데 에러가 생기면 원본데이터 손실 => 트랙잭션으로 불러올 때 실패하면 원본데이터 손실 안 가게 하기
         const reviewDoc = await transaction.get(reviewRef);
 
-        const reviews = reviewDoc.data().review || []; //review 데이터 받아오기
+        const reviews = reviewDoc.exists ? reviewDoc.data().review : [];
 
-        const newReviews = review.map((qid) => {
-          return { qid: qid, updateAt: Timestamp.now() };
+        const newReviews = review.map((item) => {
+          return {
+            qid: item.qid,
+            category: item.category,
+            updateAt: Timestamp.now(),
+          };
         }); //이번에 틀린 문제들
 
         const mergedReview = [
@@ -34,8 +65,48 @@ class Review {
         //받아온 reivews 데이터에 현재 틀린 문제들을 추가하거나 이미 틀렸던 문제이면 timestamp를 바꿈
       });
       return result;
-    } catch (error) {}
+    } catch (error) {
+      throw new Error("복습 노트 데이터를 저장하지 못했습니다.");
+    }
   };
+
+  static async getProcessedData({ bookmarks, reviews }) {
+    const processedData = [];
+    const reviewQidSet = new Set(reviews.map((review) => review.qid));
+    // 북마크와 리뷰를 비교하여 가공
+    bookmarks.forEach((bookmark) => {
+      const review = reviewQidSet.has(bookmark.qid);
+      const isWrong = review ? true : false;
+      reviewQidSet.delete(bookmark.qid); //북마크에 있는 review는 제거
+      processedData.push(
+        new Review({
+          qid: bookmark.qid,
+          updateAt: bookmark.updateAt,
+          isBookmark: true,
+          isWrong: isWrong,
+          category: bookmark.category,
+        })
+      );
+    });
+
+    const filteredReview = reviews.filter((review) =>
+      reviewQidSet.has(review.qid)
+    );
+    // 리뷰가 있지만 북마크가 없는 경우도 처리
+    filteredReview.forEach((review) => {
+      processedData.push(
+        new Review({
+          qid: review.qid,
+          updateAt: review.updateAt,
+          isBookmark: false,
+          isWrong: true,
+          category: review.category,
+        })
+      );
+    });
+
+    return processedData;
+  }
 }
 
 module.exports = { Review };
