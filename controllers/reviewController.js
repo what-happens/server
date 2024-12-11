e112c9aff8f47076c2bd76f4615a18d569ef1ac0
HwiGyoung Lee <ter0000@naver.com>
2024-12-11 20:13:15 +0900

    ✨ feat(reviewController) : 북마크와 리뷰데이터를 제공하는 api 추가

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,4 +1,24 @@
+const { Bookmark } = require("../models/bookmark");
 const { Review } = require("../models/review");
+const { compareTime } = require("../utils/comparisons");
+
+const getReview = async (req, res) => {
+  const { uid } = req.user;
+  try {
+    const reviewData = await Review.getReview(uid);
+    const bookmarkData = await Bookmark.getBookmarks(uid);
+
+    const processedData = await Review.getProcessedData({
+      bookmarks: bookmarkData,
+      reviews: reviewData,
+    });
+
+    const sortedData = processedData.sort((a, b) => compareTime(a, b));
+    res.status(200).json({ reviewNote: sortedData });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const postReview = async (req, res) => {
   const { uid } = req.user;
@@ -13,4 +33,5 @@ const postReview = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 module.exports = { getReview, postReview };
