13faa04266d1d46bbff6ff4d29f77fcabda95a04
HwiGyoung Lee <ter0000@naver.com>
2024-12-12 23:08:08 +0900

    ✨ feat(reviewController) : 복습 노트에서 오답문제를 맞출 경우 reviewnote에서 삭제하는 기능 추가

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -52,4 +52,27 @@ const postReview = async (req, res) => {
   }
 };
 
-module.exports = { getReview, postReview };
+const deleteReview = async (req, res) => {
+  const { uid } = req.user;
+  const { review } = req.body;
+
+  try {
+    const existingReview = await Review.getReview(uid);
+
+    const deleteReviewSet = new Set(...review.map((item) => item.qid));
+
+    const remainReview = existingReview.filter(
+      (review) => !deleteReviewSet.has(review.qid)
+    );
+
+    await Review.postReview(uid, remainReview);
+
+    res
+      .status(200)
+      .json({ message: "성공적으로 저장했습니다", review: mergedReview });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getReview, postReview, deleteReview };
