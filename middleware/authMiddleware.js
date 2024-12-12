3029414dac0d62681b7d2032a5b7149e6322b158
HwiGyoung Lee <ter0000@naver.com>
2024-12-12 17:28:47 +0900

    ✨ feat(authMiddleware) : accessToken 이 만료 되었을 때 갱신토큰으로 재발급 하는 로직 추가

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,40 @@
 const admin = require("firebase-admin");
+const { getIdCookieOptions } = require("../config/cookie");
 
 const authenticate = async (req, res, next) => {
   const idToken = req.cookies.accessToken;
+  const refreshToken = req.cookies.refreshToken;
 
   if (!idToken) {
     return res.status(401).json({ message: "인증되지 않은 사용자입니다." });
   }
+
   try {
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-    req.user = decodedToken;
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      req.user = decodedToken;
+    } catch (error) {
+      //id 토큰이 유효하지 않으면
+      const response = await fetch(
+        `https://securetoken.googleapis.com/v1/token?key=${process.env.FIREBASE_API_KEY}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: `grant_type=refresh_token&refresh_token=${refreshToken}`,
+        }
+      );
+
+      const json = await response.json();
+      const newIdToken = json.id_token;
+      const uid = json.user_id;
+      res.cookie("accessToken", newIdToken, getIdCookieOptions);
+      req.user = { uid: uid };
+    }
     next();
   } catch (error) {
-    return res.status(401).json({ message: "유효하지 않은 토큰입니다." });
+    return res.status(401).json({ message: "다시 로그인해주세요" });
   }
 };
 
