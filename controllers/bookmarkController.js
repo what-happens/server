081c7fa9a67e368304e7cd8e17d5490e660cc830
HwiGyoung Lee <ter0000@naver.com>
2024-12-12 23:59:02 +0900

    🎨 refactoring(bookmark) : 북마크의 모델과 컨트롤러의 책임분리

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const { Bookmark } = require("../models/bookmark");
+const { Timestamp } = require("firebase-admin/firestore");
 
 const getBookmark = async (req, res) => {
   const { uid } = req.user;
@@ -22,8 +23,39 @@ const postBookmark = async (req, res) => {
   const { uid } = req.user;
 
   try {
-    const storedBookmark = await Bookmark.postBookmark(uid, bookmark);
-    res
+    const exisingBookmark = await Bookmark.getBookmarks(uid);
+    const deleteBookmarkSet = new Set(
+      bookmark
+        .filter((item) => item.action === "delete")
+        .map((item) => item.qid)
+    );
+
+    const filteredBookmarks = exisingBookmark.filter(
+      (bookmark) => !deleteBookmarkSet.has(bookmark.qid)
+    );
+
+    const newBookmarks = bookmark
+      .filter((item) => item.action === "add")
+      .map((item) => {
+        return {
+          qid: item.qid,
+          category: item.category,
+          updateAt: Timestamp.now(),
+        };
+      });
+
+    const storedBookmark = [
+      ...new Map(
+        [...filteredBookmarks, ...newBookmarks].map((item) => [
+          item.qid,
+          { qid: item.qid, category: item.category, updateAt: item.updateAt },
+        ])
+      ).values(),
+    ];
+
+    console.log(storedBookmark);
+    await Bookmark.postBookmark(uid, storedBookmark);
+    return res
       .status(200)
       .json({ message: "북마크 업데이트 성공", bookmark: storedBookmark });
   } catch (error) {
